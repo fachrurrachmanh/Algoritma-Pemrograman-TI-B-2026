e198432ddea79302a74ae43a837701b1f70932b1
fachrurrachmanh <hkeemrach@gmail.com>
2026-02-22 01:21:10 +0700

    upload tugas

---
+++ pertemuan-2/latihan/math.py
@@ -0,0 +1,19 @@
+import myMath
+
+pertambahan = myMath.penambahan(1,2)
+print(f"hasil pertambahan ialah {pertambahan}")
+
+pembagian = myMath.pembagian(4,2)
+print(f"hasil pembagian ialah {pembagian}")
+
+perkalian = myMath.perkalian(2,2)
+print(f"hasil perkalian ialah {perkalian}")
+
+pengurangan = myMath.pengurangan(6,2)
+print(f"hasil pengurangan ialah {pengurangan}")
+
+modulus = myMath.modulus(10,2)
+print(f"hasil modulus ialah {modulus}")
+
+fibonacci = myMath.fibonacci(7)
+print(f"hasil pertambahan ialah {fibonacci}")
---
+++ pertemuan-2/latihan/myMath.py
@@ -0,0 +1,27 @@
+def penambahan (a, b):
+    tambah = a + b
+    return tambah
+
+def pengurangan (a,b):
+    kurang = a - b
+    return kurang
+
+def perkalian (a,b):
+    kali = a * b
+    return kali
+
+def pembagian (a,b):
+    bagi = a / b
+    return bagi
+
+def modulus (a,b):
+    modulus = a % b
+    return modulus
+
+def fibonacci(n):
+  if n <= 1:
+    return n
+  else:
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+ 
